7f3c62a018e2935484a63dec18253d2573c2ec9a
Juginabi <juginabi@juginabi-1215N.(none)>
2013-05-06 23:22:32 +0300

    Added gameobjectcomponent class files.

---
+++ src/GOComponent.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// C++ Standard library includes
+#include <string>
+#include <map>
+
+// Project specific headers
+#include "GameObject.h"
+
+typedef std::string goc_family_id;
+
+class GOComponent
+{
+
+};
