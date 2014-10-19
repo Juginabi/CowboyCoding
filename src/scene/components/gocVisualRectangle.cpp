bbace7318c5243743201a37d379daf5299aab021
Juginabi <jukka.vatjusanttila@gmail.com>
2014-10-19 21:12:10 +0300

    Added include to gocVisualRectangle

--- src/scene/components/gocVisualRectangle.cpp
+++ src/scene/components/gocVisualRectangle.cpp
@@ -1,4 +1,6 @@
 #include "gocVisualRectangle.h"
+// Include gameobject.h so we can query owner and use methods
+#include "../gameObject.h"
 
 const goc_type_id GocVisualRectangle::componentId_ = "GocVisualRectangle";
 
